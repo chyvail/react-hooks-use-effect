db1ee1e855656ad581c0c70338411f975303c2c2
joshua chivile <joshuachivile@gmail.com>
2023-11-03 20:42:51 +0300

    UseEffect notes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React, { useState, useEffect } from "react";
+import DogPics from "./DogPics";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+
   useEffect(() => {
     console.log("useEffect called");
-  });
+  }); // will run every time the component renders, adding dependecy array [] makes it do only once, passing variables makes it run when variable changes
+
+  useEffect(() => {
+    document.title = "Joshua"; // to update content outside React DOM
+    setTimeout(() => {
+      console.log("TimeOut here"); // useEffect also works with timeouts
+    }, 3000);
+  }, []);
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return (
+    <div>
+      <button onClick={() => setCount((count) => count + 1)}>
+        I've been clicked {count} times
+      </button>
+      <input
+        type="text"
+        placeholder="Type away..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <DogPics />
+    </div>
+  );
 }
 
 export default App;
